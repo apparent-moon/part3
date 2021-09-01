2fa8e07db4f6badf900b7c2a3a9ac8244f6ea83d
apmoon <ohappy0206@naver.com>
2021-09-02 03:13:18 +0900

    apmoon) account.java edited 20210902 3:13
    account.java를 수정하였습니다.

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -1,38 +1,89 @@
 package account;
 
-import bank.Bank;
+//import bank.Bank;
 
 import java.math.BigDecimal;
 
 public class Account {
-    //TODO: 일반 계좌 클래스의 속성은 계좌종류(N: 예금계좌, S:적금계좌), 계좌번호, 소유자, 잔액, 활성화 여부 5가지 입니다.
-    //category;             // String 계좌 종류
-    // accNo;               // String 계좌번호
-    // owner;               // String 소유자
-    // balance;             // BigDecimal 잔액
-    boolean isActive;       // boolean 활성화 여부
+    private String category;            // String 계좌 종류
+    private String accNo;               // String 계좌번호
+    private String owner;               // String 소유자
+    private BigDecimal balance;         // BigDecimal 잔액
+    private boolean isActive;           // boolean 활성화 여부
 
+    //생성자
     public Account() {
-        //TODO: 일반 계좌의 활성화 여부를 True로, 계좌 종류를 "N"(NORMAL을 의미) 설정해줍니다.
+        this.isActive = true;
+        this.category = "N";
     }
+
+    //생성자 오버로딩
     public Account(String accNo, String owner, BigDecimal balance) {
-        //TODO
+        //기본 생성자에서 제공하는 값들도 제공해야하므로 추가했음.
+        this.isActive = true;
+        this.category = "N";
+        //오버로딩하면서 받아오는 값들
+        this.accNo = accNo;
+        this.owner = owner;
+        this.balance = balance;
     }
 
-    // TODO: 일반 계좌 클래스의 각 속성에 getter/setter를 제공합니다.
+    //private으로 설정되어 외부에서 접근이 불가능하므로, getter setter를 설정했다.
+    //getter는 외부에서 값을 읽어올때 사용하고, setter는 외부에서 값을 설정할때 사용한다.
+    public String getCategory() {
+        return category;
+    }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public void setAccNo(String accNo) {
+        this.accNo = accNo;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    //return값이 void인 메소드. 계좌의 기본 정보를 return 해준다. 파라미터로 Account객체의 account를 호출한다.
     public void getAccountInfo(Account account){
-        //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
-        //계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원", account.category, account.accNo, account.owner, account.balance);
     }
 
-    public BigDecimal withdraw(BigDecimal amount) throws Exception{
-        //TODO: 출금액을 받아서 출금하는 기본 메소드입니다. this를 이용해 구현해보세요.
-
+    //BigDecimal 타입의 withdraw(출금) 메소드. 매개변수로 BigDecimal타입인 amount라는 이름의 변수를 파라미터로 받는다.
+    public BigDecimal withdraw(BigDecimal amount){
+        this.balance = this.balance.subtract(amount);
         return amount;
     }
+
+    //BigDecimal 타입의 deposit(입금) 메소드. 매개변수로 BigDecimal타입인 amount라는 이름의 변수를 파라미터로 받는다.
     public BigDecimal deposit(BigDecimal amount){
-        //TODO: 입금액을 받아서 입금하는 기본 메소드입니다. this를 이용해 구현해보세요.
+        balance = this.balance.add(amount);
         return amount;
     }
 }
