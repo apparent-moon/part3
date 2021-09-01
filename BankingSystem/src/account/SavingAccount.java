b8539df0e125134dd8dc28ec2ca355d2b4cf0787
apmoon <ohappy0206@naver.com>
2021-09-02 03:19:53 +0900

    apmoon) SavingAccount.java edited 20210902 3:19
    SavingAccount.java를 수정하였습니다.

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -4,23 +4,35 @@ import bank.Bank;
 
 import java.math.BigDecimal;
 
-//TODO: SavingAccount는 Account에서 상속을 받습니다.
+//Account 클래스에서 상속받으므로 extedns를 사용한다.
 public class SavingAccount extends Account{
+
     //TODO: 적금 계좌 클래스의 속성은 목표금액 속성을 추가로 가집니다.
     private BigDecimal goalAmount = new BigDecimal(100000);
 
+    //적금 계좌에 대한 생성자
     public SavingAccount(){
-        //TODO: 카테고리를 S로 설정해 줍니다.
+        this.setActive(true);
+        this.setCategory("S");
     }
 
+    //생성자 오버로딩
     public SavingAccount(String accNo, String owner, BigDecimal balance, BigDecimal goalAmount) {
-        // TODO
+        this.setCategory("S");
+        this.setActive(true);
+        this.setAccNo(accNo);
+        this.setOwner(owner);
+        this.setBalance(balance);
+        this.goalAmount = goalAmount;
     }
 
-    //TODO: GoalAmount getter 구현
+    //goalAmount에 대한 getter생성. setter는 이미 목표금액이 100000으로 설정되어 외부에서 수정을 할 수 없도록 선언하지 않는다.
+    public BigDecimal getGoalAmount() {
+        return goalAmount;
+    }
 
-    //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
+    //적금계좌에서는 목표금액도 보여준다.
     public void getAccountInfo(Account account) {
-        //TODO
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원", account.getCategory(), account.getAccNo(), account.getOwner(), account.getBalance(), this.getGoalAmount());
     }
 }
