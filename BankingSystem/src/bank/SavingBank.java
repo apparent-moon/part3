fed04f725e628962f8ea4bfcd12cb31f27a6ed30
apmoon <ohappy0206@naver.com>
2021-09-05 21:50:02 +0900

    apmoon) paste code (just line edited) 20210905 21:49
    line edited about Saving.java file

--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -1,11 +1,13 @@
 package bank;
 
-import account.SavingAccount;
-
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
+
+import account.Account;
+
+import account.SavingAccount;
 
 public class SavingBank extends Bank {
 
@@ -15,7 +17,6 @@ public class SavingBank extends Bank {
         try {
             BigDecimal accountBalance = account.getBalance();
             BigDecimal accountGoalAmount = account.getGoalAmount();
-
             // output 변수로 출금 할 금액을 입력받는다.
             System.out.printf("출금하실 금액을 입력해주세요 (잔액:%s원): \n", df.format(accountBalance));
             BigDecimal output = scanner.nextBigDecimal();
@@ -49,7 +50,9 @@ public class SavingBank extends Bank {
             System.out.println("입금이 완료되었습니다.\n");
             System.out.println("========================================================================");
             account.setBalance(account.getBalance().add(input));
-            System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(), account.getAccNo(), account.getOwner(), df.format(account.getBalance()), df.format(account.getGoalAmount()));
+            System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(),
+                    account.getAccNo(), account.getOwner(), df.format(account.getBalance()),
+                    df.format(account.getGoalAmount()));
             System.out.println("========================================================================");
         } catch (Exception e) {
             System.out.println("입금하는 과정에서 오류가 발생하였습니다.");
@@ -59,18 +62,19 @@ public class SavingBank extends Bank {
 
     // TODO: 목표금액을 입력받아서 SavingAccount 객체 생성하도록 재정의
     @Override
-    public SavingAccount createAccount() throws NoSuchElementException{
+    public SavingAccount createAccount() throws NoSuchElementException {
         try {
             System.out.print("적금계좌명 혹은 적금계좌 소유자의 이름을 입력해주세요: ");
             String owner = scanner.next();
             String ano = String.format(new DecimalFormat("0000").format(++seq));
-            System.out.print("목표 금액을 설정해주세요: " );
+            System.out.print("목표 금액을 설정해주세요: ");
             BigDecimal goal = scanner.nextBigDecimal();
-            System.out.printf("\n %s 계좌가 발급되었습니다.\n", owner);
-            //Account 생성자를 호출해서 account라는 객체를 생성했다.
+            System.out.printf("%s 계좌가 발급되었습니다.\n", owner);
+            System.out.println("=====================================================");
+            // Account 생성자를 호출해서 account라는 객체를 생성했다.
             SavingAccount account = new SavingAccount(ano, owner, new BigDecimal("0"), goal);
-            return account ;
-        }catch (Exception e){
+            return account;
+        } catch (Exception e) {
             System.out.println("계좌를 생성하는 과정에서 오류가 발생하였습니다.");
             return null;
         }
