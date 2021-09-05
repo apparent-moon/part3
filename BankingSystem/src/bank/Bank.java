cc2c4b61ab0ac9aa57acb88d8518ac073f922f70
apmoon <ohappy0206@naver.com>
2021-09-05 17:48:41 +0900

    apmoon) withdraw method added 20210905 17:48
    withdraw method added

--- BankingSystem/src/bank/Bank.java
+++ BankingSystem/src/bank/Bank.java
@@ -16,26 +16,66 @@ public class Bank {
     public static DecimalFormat df = new DecimalFormat("#,###");
 
     // 뱅킹 시스템의 기능들
+    // 출금 메소드 withdraw.
     public void withdraw() throws Exception {
-        //TODO: 출금 메서드 구현
-        //TODO: key, value 형태의 HashMap을 이용하여 interestCalculators 구현
-        //여기서 key: category, value: 각 category의 InterestCalculator 인스턴스
 
-        // 계좌번호 입력
+        // TODO: key, value 형태의 HashMap을 이용하여 interestCalculators 구현
+        // key: category
+        // value: category의 InterestCalculator 인스턴스
+        HashMap<String, InterestCalculator> interestCalculators = new HashMap<>();
+        interestCalculators.put("N", new BasicInterestCalculator());
+
         Account account;
-        while(true){
-            System.out.println("\n출금하시려는 계좌번호를 입력하세요.");
-            String accNo = scanner.next();
-            // TODO: 검색 -> 적금 계좌이면 적금 계좌의 출금 메소드 호출 -> 완료시 break
+        while (true) {
+            // 계좌번호 입력
+            System.out.println("\n출금하실 계좌의 계좌번호를 입력하세요: ");
+            String accountNum = scanner.next();
+            if (findAccount(accountNum) != null) {
+                // 계좌를 찾아서 account 변수에 집어넣음
+                account = findAccount(accountNum);
 
+                // 만약 적금계좌 타입의 계좌가 입력되었다면 SavingBank.java의 메소드를 실행한다.
+                //Q. 질문 여기서 this가 가리키는게 account가 맞는지??? 왜 this밖에 안되는건지 이해가 안감!
+                if (account.getCategory().equals("S")) {
+                    SavingBank bank = (SavingBank) this;
+                    bank.withdraw((SavingAccount) account); // == ((SavingBank) this).withdraw((SavingAccount) account);
+                    return;
+                }
+                break;
+            } else {
+                System.out.println("존재하지 않는 계좌번호입니다. 계좌번호를 다시 입력해주세요.");
+                // 재귀호출을 통해서 올바른 값이 들어올때까지 해당 과정을 진행한다.
+                withdraw();
+            }
         }
-        // 출금처리
-        System.out.println("\n출금할 금액을 입력하세요.");
-        // TODO: interestCalculators 이용하 이자 조회 및 출금
+
         try {
+            BigDecimal accountBalance = account.getBalance();
 
-        }catch (Exception e){
+            // output 변수로 출금 할 금액을 입력받는다.
+            System.out.printf("출금하실 금액을 입력해주세요 (잔액:%s원): \n", df.format(accountBalance));
+            BigDecimal output = scanner.nextBigDecimal();
 
+            // 만약 출금 하려는 금액이 잔액보다 클 경우
+            if (accountBalance.compareTo(output) < 0) {
+                System.out.println("해당 금액은 출금할 수 없는 금액입니다. 처음 화면으로 돌아갑니다.\n");
+                System.out.println("=====================================================");
+                System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원\n", account.getCategory(),
+                        account.getAccNo(), account.getOwner(), df.format(accountBalance));
+                System.out.println("=====================================================");
+            } else {
+                BigDecimal interest = interestCalculators.get("N").getInterest(accountBalance);
+                System.out.println("현재 " + account.getAccNo() + " 계좌에 지급되어있는 이자는 " + df.format(interest) + "원 입니다.");
+                account.setBalance(account.getBalance().add(interest));
+                System.out.printf(output + "원 출금이 완료되었습니다.\n");
+                account.setBalance(account.getBalance().subtract(output));
+                System.out.println("=====================================================");
+                System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 \n", account.getCategory(), account.getAccNo(), account.getOwner(), df.format(account.getBalance()));
+                System.out.println("=====================================================");
+            }
+        } catch (Exception e) {
+            System.out.println("출금하는 과정에서 오류가 발생하였습니다.");
+            return;
         }
     }
 
--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -4,13 +4,42 @@ import account.SavingAccount;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class SavingBank extends Bank {
 
-    public void withdraw(SavingAccount account) throws Exception{
-        // TODO: Account의 출금 메서드에서 잔액/목표 금액 체크하여 조금 다르게 구현
-        // throws Exception 적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.
+    public void withdraw(SavingAccount account) throws Exception {
+        HashMap<String, InterestCalculator> interestCalculators = new HashMap<>();
+        interestCalculators.put("S", new SavingInterestCalculator());
+        try {
+            BigDecimal accountBalance = account.getBalance();
+            BigDecimal accountGoalAmount = account.getGoalAmount();
+
+            // output 변수로 출금 할 금액을 입력받는다.
+            System.out.printf("출금하실 금액을 입력해주세요 (잔액:%s원): \n", df.format(accountBalance));
+            BigDecimal output = scanner.nextBigDecimal();
+
+            // 만약 출금 하려는 금액이 잔액보다 클 경우
+            if (accountGoalAmount.compareTo(output) > 0) {
+                System.out.println("목표금액을 달성해야 출금이 가능합니다. 처음 화면으로 돌아갑니다.\n");
+                System.out.println("=====================================================");
+                System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(), account.getAccNo(), account.getOwner(), df.format(accountBalance), df.format(accountGoalAmount));
+                System.out.println("=====================================================");
+            } else {
+                BigDecimal interest = interestCalculators.get("S").getInterest(accountBalance);
+                System.out.println("현재 " + account.getAccNo() + " 계좌에 지급되어있는 이자는 " + df.format(interest) + "원 입니다.");
+                account.setBalance(account.getBalance().add(interest));
+                System.out.printf(output + "원 출금이 완료되었습니다.\n");
+                account.setBalance(account.getBalance().subtract(output));
+                System.out.println("=====================================================");
+                System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(), account.getAccNo(), account.getOwner(), df.format(account.getBalance()));
+                System.out.println("=====================================================");
+            }
+        } catch (Exception e) {
+            System.out.println("출금하는 과정에서 오류가 발생하였습니다.");
+            return;
+        }
     }
 
     public void deposit(SavingAccount account) throws Exception {
@@ -20,9 +49,7 @@ public class SavingBank extends Bank {
             System.out.println("입금이 완료되었습니다.\n");
             System.out.println("========================================================================");
             account.setBalance(account.getBalance().add(input));
-            System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(),
-                    account.getAccNo(), account.getOwner(), df.format(account.getBalance()),
-                    df.format(account.getGoalAmount()));
+            System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표금액 : %s원\n", account.getCategory(), account.getAccNo(), account.getOwner(), df.format(account.getBalance()), df.format(account.getGoalAmount()));
             System.out.println("========================================================================");
         } catch (Exception e) {
             System.out.println("입금하는 과정에서 오류가 발생하였습니다.");
